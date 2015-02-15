88cb438a61e4266760f63a242e9ad8869f0590f0
Andrew Darqui <andrew.darqui@gmail.com>
2015-02-15 00:17:54 -0500

    mimics tibbe examples: https://github.com/tibbe/ekg-core/blob/master/benchmarks/Counter.hs#L22

--- examples/simple/simple.go
+++ examples/simple/simple.go
@@ -1,16 +1,25 @@
+// Perform 100,000 atomic increments using 100 concurrent writers.
 package main
 
 import (
     "github.com/adarqui/ekg-core-go"
     "fmt"
+    "time"
 )
 
 func main() {
     store := ekg_core.New()
     counter := store.CreateCounter("test_counter")
-    counter.Inc()
-    fmt.Println(counter.Read())
-    counter.Add(10)
+
+    for n := 1; n <= 100; n++ {
+        go func() {
+            for iters := 1 ; iters <= 100000 ; iters++ {
+                counter.Inc()
+            }
+        }()
+    }
+
+    time.Sleep(2 * time.Second)
     fmt.Println(counter.Read())
     fmt.Println("done.")
 }
